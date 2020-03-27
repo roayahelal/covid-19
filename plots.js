c88c179a5b16e4082e0652203b9fb37ce16a7f6e
roayahelal <56054235+roayahelal@users.noreply.github.com>
2020-03-26 23:42:56 -0400

    updated date function

--- plots.js
+++ plots.js
@@ -12,7 +12,7 @@
         }, 0);
         console.log(weighted_mortality_per_country);
         var dates = data.map(row=> (row.Date)).slice(2,3);
-        var date =Date.parse(dates);
+        var date = new Date();
         console.log(`Date Updated: ${(data.map(row=> (row.Date)).slice(2,3))}`)
         d3.select("#date-time").text(date);
 
