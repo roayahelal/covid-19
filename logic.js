c685164341caa623f7b13234cd6550a84e01161e
roayahelal <56054235+roayahelal@users.noreply.github.com>
2020-03-26 23:50:27 -0400

    added forcasted mortality rate

--- logic.js
+++ logic.js
@@ -44,7 +44,7 @@ d3.json("data.json", function init(x) {
     var intMortRate = (( +x[i].TotalDeaths / +x[i].TotalCases ));
     var totMortRate = intMortRate + totMortRate;
     var avgMortRate = totMortRate/(i+1);
-    var closedMortRate = totDeaths / (totDeaths + totRecovered)
+    var closedMortRate = totDeaths / (totDeaths + totRecovered);
 
     var coords = [x[i].Latitude, x[i].Longitude]
     
@@ -136,8 +136,8 @@ function specifyCountry() {
           "Total Deaths: ": Deaths.toLocaleString(),
           "24h Cases: ": NewCases.toLocaleString(),
           "24h Deaths: ": NewDeaths.toLocaleString(),
-          "Current Mortalitiy Rate: ": MortRate
-          "Forcasted Mortality Rate: "
+          "Current Mortalitiy Rate: ": MortRate,
+          "Forcasted Mortality Rate: " closedMortRate
         };
         console.log(countryData);
         
