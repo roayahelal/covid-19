8fa9a840cd5016cf1ebbcc68ba00f702a09fb953
roayahelal <56054235+roayahelal@users.noreply.github.com>
2020-03-29 02:03:26 -0400

    total deaths plot is not correct due to sorting

--- plots.js
+++ plots.js
@@ -49,40 +49,40 @@
       
       Plotly.newPlot("plot", plotData, layout)});
 
-// total deaths chart
-      d3.json("data.json", function(data) {
-        var data = data;
-        var trace1 = {
-          x: data.map(row => row.Country),
-          y: data.map(row => row.TotalDeaths).slice(0,10),
-          type: "bar",
-          text: data.map(row => row.TotalDeaths),
-          textposition: 'auto',
-          // hoverinfo: 'none',
-          marker: {
-            color: 'rgb(128,0,0)',
-            opacity: 0.7,
-            line: {
-              color: 'rgb(0,0,0)',
-              width: 1.5}
-            },
-          transforms: [{
-            type: 'sort',
-            target: 'y',
-            order: 'descending'},
-            {type: 'filter',
-            target: 'y',
-            operation: '>',
-            value: 1
-          }]
+// // total deaths chart
+//       d3.json("data.json", function(data) {
+//         var data = data;
+//         var trace1 = {
+//           x: data.map(row => row.Country),
+//           y: data.map(row => row.TotalDeaths).slice(0,10),
+//           type: "bar",
+//           text: data.map(row => row.TotalDeaths),
+//           textposition: 'auto',
+//           // hoverinfo: 'none',
+//           marker: {
+//             color: 'rgb(128,0,0)',
+//             opacity: 0.7,
+//             line: {
+//               color: 'rgb(0,0,0)',
+//               width: 1.5}
+//             },
+//           transforms: [{
+//             type: 'sort',
+//             target: 'y',
+//             order: 'descending'},
+//             {type: 'filter',
+//             target: 'y',
+//             operation: '>',
+//             value: 1
+//           }]
           
-        };
-        var plotData = [trace1];
+//         };
+//         var plotData = [trace1];
         
-        var layout = {
-          title: "Top Total Deaths Per Country"
-        };
+//         var layout = {
+//           title: "Top Total Deaths Per Country"
+//         };
           
-        Plotly.newPlot("plot2", plotData, layout);
+//         Plotly.newPlot("plot2", plotData, layout);
 
-      });
+// });
